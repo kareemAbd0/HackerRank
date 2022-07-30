153aab18564bf90d5c72298ef062e2f0a9264ff4
Kareem Abdo <[email>
2022-07-31 00:30:34 +0200

    save q

---
+++ Week 1 -DS/2D array -DS/cmake-build-debug/CMakeFiles/2D_array__DS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2D_array__DS.exe"
+  "2D_array__DS.exe.manifest"
+  "2D_array__DS.pdb"
+  "CMakeFiles/2D_array__DS.dir/main.c.obj"
+  "lib2D_array__DS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2D_array__DS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
