768bcaf87c49111434e63fffc1e7f428320adfd8
Kareem Abdo <[email>
2022-07-30 21:23:29 +0200

    save q

---
+++ week 1 -conditional/loops/cmake-build-debug/CMakeFiles/loops.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loops.dir/main.c.obj"
+  "libloops.dll.a"
+  "loops.exe"
+  "loops.exe.manifest"
+  "loops.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/loops.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
