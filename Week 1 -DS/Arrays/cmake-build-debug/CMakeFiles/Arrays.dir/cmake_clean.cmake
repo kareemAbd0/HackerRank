031161edfc16ca05b3079a4b9c9b3fde75bef090
Kareem Abdo <[email>
2022-07-30 19:36:48 +0200

    first

---
+++ Week 1 -DS/Arrays/cmake-build-debug/CMakeFiles/Arrays.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Arrays.exe"
+  "Arrays.exe.manifest"
+  "Arrays.pdb"
+  "CMakeFiles/Arrays.dir/main.c.obj"
+  "libArrays.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Arrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
