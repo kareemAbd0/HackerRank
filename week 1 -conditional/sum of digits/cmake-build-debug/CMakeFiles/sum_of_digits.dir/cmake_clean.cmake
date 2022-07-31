ce9f54fc417e873fd26d8ae3524e45eee24fa69c
Kareem Abdo <[email>
2022-07-31 16:35:37 +0200

    adding

---
+++ week 1 -conditional/sum of digits/cmake-build-debug/CMakeFiles/sum_of_digits.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sum_of_digits.dir/main.c.obj"
+  "libsum_of_digits.dll.a"
+  "sum_of_digits.exe"
+  "sum_of_digits.exe.manifest"
+  "sum_of_digits.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sum_of_digits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
