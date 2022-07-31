fa41cd1f410500fa57954774ffabe77e750373f4
Kareem Abdo <[email>
2022-07-31 19:42:00 +0200

    adding

---
+++ Week 1 -DS/sparse arrays/cmake-build-debug/CMakeFiles/sparse_arrays.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sparse_arrays.dir/main.c.obj"
+  "libsparse_arrays.dll.a"
+  "sparse_arrays.exe"
+  "sparse_arrays.exe.manifest"
+  "sparse_arrays.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sparse_arrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
