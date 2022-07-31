86ce7f82f411e14203c2417360eaa19c2fe7b933
Kareem Abdo <[email>
2022-07-31 15:51:26 +0200

    adding

---
+++ Week 1 -DS/Leftt rotations/cmake-build-debug/CMakeFiles/Leftt_rotations.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Leftt_rotations.dir/main.c.obj"
+  "Leftt_rotations.exe"
+  "Leftt_rotations.exe.manifest"
+  "Leftt_rotations.pdb"
+  "libLeftt_rotations.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Leftt_rotations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
